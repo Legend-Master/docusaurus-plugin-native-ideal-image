19b5e1a8d57fd39b634e4c3b4c425100504ba3bd
Tony <legendmastertony@gmail.com>
2024-08-11 15:54:10 +0800

    Clean up

--- src/theme/NativeIdealImage.tsx
+++ src/theme/NativeIdealImage.tsx
@@ -4,53 +4,32 @@ import type { NativeIdealImageProps } from '../index.js'
 import styles from './NativeIdealImage.module.css'
 
 export default function IdealImage(props: NativeIdealImageProps): JSX.Element {
-	const { img, ...propsRest } = props
+	const { img, sizes, loading, ...propsRest } = props
 
-	// In dev env just use regular img with original file
-	if (typeof img === 'string' || 'default' in img) {
-		return <img src={typeof img === 'string' ? img : img.default} loading="lazy" {...propsRest} />
-	}
+	const formats = typeof img === 'object' && 'formats' in img ? img.formats : []
+	const lqip = typeof img === 'object' && 'lqip' in img ? img.lqip : ''
 
 	return (
 		<picture
 			className={styles.picture}
 			style={
 				{
-					'--lqip': `url(${img.lqip})`,
-					// backgroundImage: `url(${img.lqip})`,
-					// backgroundRepeat: 'no-repeat',
-					// backgroundSize: 'cover',
-					// width: '100%',
-					// height: 'auto',
+					'--lqip': `url(${lqip})`,
 				} as React.CSSProperties
 			}
 		>
-			{img.formats.map((format) => (
+			{formats.map((format) => (
 				<source
-					// media={`(min-width: ${source.width})`}
-					// sizes={`source.width`}
-					// srcSet={source.fileName}
 					srcSet={format.srcSet.map((image) => `${image.path} ${image.width}w`).join(',')}
-					sizes="auto"
 					type={format.mime}
 					key={format.mime}
 				/>
 			))}
 			<img
-				loading="lazy"
-				// src={img.src.fileName}
-				// width={img.src.width}
-				// height={img.src.height}
-				// style={
-				// 	{
-				// 		'--lqip': `url(${img.lqip})`,
-				// 		// backgroundImage: `url(${img.lqip})`,
-				// 		// backgroundRepeat: 'no-repeat',
-				// 		// backgroundSize: 'cover',
-				// 		// width: '100%',
-				// 		// height: 'auto',
-				// 	} as React.CSSProperties
-				// }
+				// For disableInDev
+				src={typeof img === 'string' ? img : 'default' in img ? img.default : undefined}
+				loading={loading ?? 'lazy'}
+				sizes={sizes ?? 'auto'}
 				{...propsRest}
 			/>
 		</picture>
