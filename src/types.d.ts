19035dd50fd32fbb4808c8ff522ada95442d4aa6
Tony <legendmastertony@gmail.com>
2024-08-14 20:58:12 +0800

    Point types.d.ts's imports to /lib/index.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,10 @@
 declare module 'ideal-img!*' {
-	const x: import('./index.ts').NativeIdealImageData
+	const x: import('../lib/index.d.ts').NativeIdealImageData
 	export default x
 }
 
 declare module '@theme/NativeIdealImage' {
 	export default function NativeIdealImage(
-		props: import('./index.ts').NativeIdealImageProps,
+		props: import('../lib/index.d.ts').NativeIdealImageProps,
 	): JSX.Element
 }
