6a7b0241155036aa781793ff1f92d0ba61f302df
Tony <legendmastertony@gmail.com>
2026-06-28 09:40:42 +0800

    Update dev docusaurus to 3.10.1 and ts to v6

--- example/docusaurus.config.ts
+++ example/docusaurus.config.ts
@@ -18,15 +18,19 @@ const config: Config = {
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/docusaurus-plugin-native-ideal-image',
 
-	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
-
 	// Even if you don't use internationalization, you can use this field to set
 	// useful metadata like html lang. For example, if your site is Chinese, you
 	// may want to replace "en" with "zh-Hans".
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en'],
+	},
+
+	future: {
+		v4: {
+			mdx1CompatDisabledByDefault: true,
+			removeLegacyPostBuildHeadAttribute: true,
+		},
 	},
 
 	presets: [
